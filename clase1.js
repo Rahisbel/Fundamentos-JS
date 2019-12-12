2bb7b4bd4b1d1aace6badc826e70f6b0ec10884a
Rahisbel <rahisbel.herrera@unet.edu.ve>
2019-12-11 20:12:01 -0400

    Primer Hola Mundo, creacion de primeras variables

---
+++ clase1.js
@@ -0,0 +1,16 @@
+/**
+ *
+ * Declaración de variables
+ * JS es un lenguaje debilmente tipado
+ * por lo que las variables pueden tener 
+ * cualquier  tipo de dato
+ */
+
+
+var nombre   = 'Rahisbel' 
+var apellido = 'Herrera'
+var edad = 22 
+edad = '22 años'
+
+console.log('Hola ' + nombre + ' ' + apellido)
+console.log('tengo ' + edad)
