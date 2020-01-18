12eda4455bc93d45367f93d2aa06c466c8c3ff11
Rahisbel <rahisbel.herrera@unet.edu.ve>
2020-01-18 16:44:18 -0400

    objetos

--- clase1.js
+++ clase1.js
@@ -9,8 +9,8 @@ var apellido = 'Herrera'
 var edad = 22 /* edad = '22 años' */
 
 
-console.log('Hola ' + nombre + ' ' + apellido)
-console.log('tengo ' + edad)
+//console.log('Hola ' + nombre + ' ' + apellido)
+//console.log('tengo ' + edad)
 
 /**
  * Declaración de variables
@@ -84,13 +84,13 @@ var nombre = 'Rahisbel' , edad = 22
 
 function imprimirEdad(n,e){
 	
-	console.log(`${n} tiene ${e} años`)
+	//console.log(`${n} tiene ${e} años`)
 }
 
-imprimirEdad(nombre,edad) //por ser un lenguaje fuertemente tipado 
-imprimirEdad('vicky',28)	  // en caso de que no enviemos los parametros	
-imprimirEdad('Eric',24)	  // no lanzará error, tratará de ejecutarlo
-imprimirEdad('Dario',27)
+//imprimirEdad(nombre,edad) //por ser un lenguaje fuertemente tipado 
+//imprimirEdad('vicky',28)	  // en caso de que no enviemos los parametros	
+//imprimirEdad('Eric',24)	  // no lanzará error, tratará de ejecutarlo
+//imprimirEdad('Dario',27)
 
 
 
@@ -100,9 +100,64 @@ imprimirEdad('Dario',27)
 
 function imprimirNombreEnMayusculas(n){ 
 	n = n.toUpperCase()
-	console.log(n)
+	//console.log(n)
 	// para no modificar la variable nombre (MAYUS)
 	// se utiliza un parametro n o nombre, cualquiera
 }
 
 imprimirNombreEnMayusculas(nombre)
+
+
+
+/*==========================================
+=            clase 6: Objetos              =
+==========================================*/
+
+/*Los objetos se definen delimitados mediante llaves {}
+
+Un atributo se compone de una clave (key) y un valor (value), 
+que se separan entre sí por dos puntos “”:"". 
+Los valores pueden ser de tipo string, número, booleano, etc. 
+Cada atributo está separado del siguiente por una coma. 
+Un objeto puede tener todos los atributos que sean necesarios.
+
+Escribir el nombre de un objeto separado por un punto del nombre de un atributo, 
+nos permite acceder al valor de dicho atributo para ese objeto. 
+Un objeto también se puede pasar como atributo en una función.
+
+Las últimas versiones de JavaScript nos permiten desglosar el objeto 
+para acceder únicamente al atributo que nos interesa.
+Esto se consigue encerrando el nombre del atributo entre llaves { }.
+*/
+
+var rahisbel = {
+	nombre : 'Rahisbel',
+	apellido : 'Herrera',
+	edad : 22
+}
+
+var dario = {
+	nombre : 'Dario',
+	apellido : 'Susnisky',
+	edad : 27
+}
+
+function imprimirNombreEnMayusculas1(persona){ 
+	var nombre = persona.nombre.toUpperCase()
+	//console.log(nombre)
+	//console.log(persona.nombre.toUpperCase())
+}
+
+// de ésta manera obtenemos el dato de una vez
+function imprimirApellido({apellido}){
+	//console.log(apellido.toUpperCase())
+}
+
+/*en caso de que la función no reciba como parámetro
+un objeto, se coloca rahisbel.nombre, dario.nombre  */
+//imprimirNombreEnMayusculas1(rahisbel)
+//imprimirNombreEnMayusculas1(dario)
+//imprimirApellido(rahisbel)
+
+// define el objeto y asigna nuevo nombre
+//imprimirNombreEnMayusculas1({nombre : 'pepito'})
