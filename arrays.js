51f08fe020fd76085cd748c635bbf1be88ab847f
Rahisbel <ryhn236@gmail.com>
2020-01-21 17:18:22 -0400

    filtrar un array

--- arrays.js
+++ arrays.js
@@ -31,3 +31,33 @@ for(var i = 0; i < personas.length; i++){
 
 	console.log(`${persona.nombre} mide ${persona.altura} m`)
 }
+
+/*===================================================
+=            clase 18 : Filtrar un array            =
+===================================================*/
+
+/* En esta clase aprenderemos como filtrar los arrays. 
+Para filtrar siempre necesitamos establecer una condición. 
+En este ejemplo nuestra condición es que la estatura de las personas sea mayor de 1.80mts.
+
+El método filter ( ) crea una nueva matriz con todos los elementos que pasan la prueba 
+implementada por la función proporcionada.
+
+Recuerda que si no hay elementos que pasen la prueba, filter devuelve un array vacío.
+*/
+
+//const esAlta = persona => persona.altura > 1.6
+const esAlta = ({altura}) => altura > 1.6
+
+var personasAltas = personas.filter(esAlta)
+/*var personasAltas = personas.filter(function (persona){
+	return persona.altura > 1.6
+})*/
+
+//console.log(personasAltas)
+
+/* Reto: escribe el filtrado de personas bajas. 
+
+const esBaja = ({altura}) => altura <= 1.5
+var personasBajas = personas.filter(esBaja)
+console.log(personasBajas) */
