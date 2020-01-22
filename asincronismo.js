40eba3fbaa08a0f59f319ec3f8b83c7d2300d586
Rahisbel <ryhn236@gmail.com>
2020-01-22 14:50:15 -0400

    ¿Cómo funciona el tiempo en JS?

--- asincronismo.js
+++ asincronismo.js
@@ -17,3 +17,16 @@ Si se acumulan funciones en la cola de tareas y JavaScript se encuentra ejecutan
 procesos muy pesados, el EventLoop quedará bloqueado y esas funciones pudieran 
 tardar demasiado en ejecutarse.
 */
+
+/*=================================================================
+=            clase 28: ¿Cómo funciona el tiempo en JS?            =
+=================================================================*/
+
+/* En principio, cualquier tarea que se haya delegado al navegador 
+a través de un callback, deberá esperar hasta que todas las instrucciones 
+del programa principal se hayan ejecutado. Por esta razón el tiempo de 
+espera definido en funciones como setTimeout, no garantizan que el callback 
+se ejecute en ese tiempo exactamente, sino en cualquier momento a partir de allí, 
+sólo cuando la cola de tareas se haya vaciado. 
+*/
+
