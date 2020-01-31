efe9bf3dadc008bc1219adb39474ba5a9e0ba49b
Rahisbel <ryhn236@gmail.com>
2020-01-31 09:21:01 -0400

    cambiando el contexto al llamar a una función

--- complementos.js
+++ complementos.js
@@ -175,3 +175,34 @@ const cumpleañosInmutable = persona => ({
 	edad : persona.edad + 1
 })
 
+/*=====================================================================
+=            Cambiando de contexto al llamar a una función            =
+=====================================================================*/
+
+/* 
+El contexto (o alcance) de una función es por lo general, window. Así que en ciertos casos, 
+cuando intentamos referirnos a this en alguna parte del código, es posible que tengamos un 
+comportamiento inesperado, porque el contexto quizás no sea el que esperamos.
+
+Existen al menos tres maneras de cambiar el contexto de una función.
+
+Usando el método .bind, enviamos la referencia a la función sin ejecutarla, pasando el contexto como parámetro.
+Usando el método .call, ejecutamos inmediatamente la función con el contexto indicado
+Usando el método .apply, es similar a .call pero los parámetros adicionales se pasan como un arreglo de valores
+*/
+
+const yesika = {
+	nombre:'Yesika'
+	apellido :'Cortes'
+}
+
+function saludar (saludo = 'Hola'){
+	console.log(`${saludo} mi nombre es ${this.nombre}`)
+}
+								//contexto de quien será this, otro parametros
+//const saludarARahisbel = saludar.bind(rahisbel)
+//const saludarAYesika = saludar.bind(yesika)
+//setTimeout(saludar.bind(rahisbel,'Hola che'),1000)
+
+saludar.call(rahisbel,'Hola che') // la ejecuta de una vez
+saludar.apply(rahisbel,['Hola che'])
