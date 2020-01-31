c4031e57173d9565997ae9c4aae1b353e2a75713
Rahisbel <ryhn236@gmail.com>
2020-01-31 09:02:54 -0400

    clousures de JS

--- complementos.js
+++ complementos.js
@@ -128,3 +128,27 @@ function factorial(n){
 	return this.cache[n]
 
 }
+
+/*===========================================================
+=            Entiende los closures de JavaScript            =
+===========================================================*/
+
+/* Un closure, básicamente, es una función que recuerda el estado 
+de las variables al momento de ser invocada, y conserva este estado 
+a través de reiteradas ejecuciones. Un aspecto fundamental de los 
+closures es que son funciones que retornan otras funciones.
+*/
+
+function crearSaludo(finalDeFrase){
+	return function(nombre){
+		console.log(`Hola ${nombre} ${finalDeFrase}`)
+	}
+}
+
+const saludoArgentino = crearSaludo('che')
+const saludoMexicano = crearSaludo('guey')
+const saludoColombiano = crearSaludo('parcero')
+
+saludoArgentino('Rahisbel')
+saludoMexicano('Rahisbel')
+saludoColombiano('Rahisbel')
