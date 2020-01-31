088a4a07ba5f74dd1c54b2e5d21ff96a675fd918
Rahisbel <ryhn236@gmail.com>
2020-01-31 08:56:13 -0400

    ahorrando cómputo

--- complementos.js
+++ complementos.js
@@ -92,5 +92,39 @@ function divisionEntera(dividendo,divisor){
 	}
 
 	return 1 + divisionEntera(dividendo-divisor,divisor)
-	
+
+}
+
+
+/*========================================================
+=            Memorización : Ahorrando cómputo            =
+========================================================*/
+
+/* 
+La memoización es una técnica de programación que nos permite 
+ahorrar cómputo o procesamiento en JavaScript, al ir almacenando 
+el resultado invariable de una función para que no sea necesario 
+volver a ejecutar todas las instrucciones de nuevo, cuando se vuelva
+a llamar con los mismos parámetros. Es similar a usar memoria cache.
+*/
+
+
+function factorial(n){
+	if(!this.cache){
+		this.cache = {}
+	}
+
+	//debugger
+	if(this.cache[n]){
+		return this.cache[n]
+	}
+
+	if(n==1){ // caso base
+		return 1
+	}
+
+	this.cache[n] = n * factorial(n-1)
+	//debugger
+	return this.cache[n]
+
 }
